1bd1aa3d0f19d3b36e2349ab746f2d2ed5cd40cd
kangrae <kangrae0501@gmail.com>
2025-10-18 02:07:17 +0900

    feat: 문자열의 숫자를 계산하는 메서드 구현
    - 문자열의 숫자를 기본 구분자와 커스텀 구분자로 구분하여 각각을 합한 값을 반환

--- src/main/java/calculator/Expression.java
+++ src/main/java/calculator/Expression.java
@@ -12,6 +12,25 @@ public class Expression {
         this.expression = validateNumbers(input);
     }
 
+    public Integer makeSum() {
+        String defaultSeparator = ",:";
+        String separator = defaultSeparator;
+        if (customSeparator.hasSeparator()) {
+            separator += customSeparator.getSeparator();
+        }
+
+        String splitRegex = "[" + Pattern.quote(separator) + "]";
+
+        int sum = 0;
+        for (String number : expression.split(splitRegex)) {
+            if (!number.isEmpty()) {
+                sum += Integer.parseInt(number);
+            }
+        }
+
+        return sum;
+    }
+
     private String validateNumbers(String input) {
         String defaultSeparator = ",:";
         String separator = defaultSeparator;
@@ -22,8 +41,6 @@ public class Expression {
         }
 
         String numberPattern = "^([0-9]+(?:[" + Pattern.quote(separator) + "][0-9]+)*)?$";
-        System.out.println(numberPattern);
-
         if (!input.matches(numberPattern)) {
             throw new IllegalArgumentException("[ERROR] 잘못된 숫자 입력 형식입니다.");
         }
