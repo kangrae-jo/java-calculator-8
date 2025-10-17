730d4d70b9e41910100009b5709679d8c7f7d1ea
kangrae <kangrae0501@gmail.com>
2025-10-18 02:08:05 +0900

    feat: 사용자 입력과 결과 출력 기능 구현
    - 문자열 입력과 계산된 결과를 출력함

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Expression expression = new Expression(Console.readLine());
+
+        System.out.println("결과 : " + expression.makeSum());
     }
+
 }
