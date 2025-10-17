b44d7d527636738cbde029ee308e1b001274d41d
kangrae <kangrae0501@gmail.com>
2025-10-18 01:52:21 +0900

    feat: 커스텀 구분자 클래스 구현
    - 커스텀 구분자는 커스텀 구분자 등록 형식을 판단한다.
    - 문제가 없다면 커스텀 구분자를 추출하여 등록한다.

---
+++ src/main/java/calculator/CustomSeparator.java
@@ -0,0 +1,39 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomSeparator {
+
+    private final Character separator;
+
+    public CustomSeparator(String input) {
+        this.separator = validateRegex(input);
+    }
+
+    public boolean hasSeparator() {
+        return separator != null;
+    }
+
+    public Character getSeparator() {
+        return separator;
+    }
+
+    private Character validateRegex(String input) {
+        // 헤더가 없는 경우
+        if (input.isEmpty() || input.charAt(0) != '/') {
+            return null;
+        }
+
+        // 헤더가 있는 경우
+        String headerPattern = "^//(\\D)\\\\n.*$";
+        Pattern pattern = Pattern.compile(headerPattern);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 커스텀 구분자 등록 형식입니다. 어플리케이션을 종료합니다.");
+        }
+
+        return matcher.group(1).charAt(0);
+    }
+
+}
