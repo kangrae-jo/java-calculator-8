7376cac0ccc83281cee308f4dcec4c18fabdc859
kangrae <kangrae0501@gmail.com>
2025-10-20 10:50:54 +0900

    feat: 시작 문구 출력 기능 구현 및 scanner자원 회수
    - Console.readLine()으로 열린 scanner를 반환한다.
    - 문제 실행 결과를 정확히 맞추기 위해서 시작 문구를 출력한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,8 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
 
     public static void main(String[] args) {
-        Expression expression = new Expression(Console.readLine());
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        Console.close();
 
+        Expression expression = new Expression(input);
         System.out.println("결과 : " + expression.makeSum());
     }
 
